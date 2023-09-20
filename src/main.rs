618121d2c869d47e534eed889951189ac6bb2acb
Yury Cavalcante <yurypcfarias@gmail.com>
2023-09-20 18:53:39 -0300

    chore: using postgres repository in the webserver

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use std::{collections::HashMap, sync::Arc};
-
+use std::{sync::Arc, env, net::SocketAddr};
 use axum::{
   routing::{get, post},
   Router,
   response::IntoResponse,
-  http::StatusCode, extract::{State, Path}, Json,
+  http::StatusCode, extract::{State, Path, Query}, Json,
 };
-
-use tokio::sync::RwLock;
+use persistence::PostgresRepository;
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
-use time::{Date, macros::date};
+use time::Date;
+
+mod persistence;
 
 time::serde::format_description!(date_format, Date, "[year]-[month]-[day]");
 
-#[derive(Serialize, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, sqlx::FromRow)]
 pub struct Person {
   pub id: Uuid,
   #[serde(rename = "nome")]
@@ -26,14 +26,25 @@ pub struct Person {
   pub stack: Option<Vec<String>>,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct NewPerson {
+  #[serde(rename = "nome")]
+  pub name: NewPersonName,
+  #[serde(rename = "apelido")]
+  pub nick: NewPersonNick,
+  #[serde(rename = "nascimento", with = "date_format")]
+  pub birth_date: Date,
+  pub stack: Option<Vec<Tech>>,
+}
+
+#[derive(Serialize, Debug, Deserialize, Clone)]
 pub struct NewPersonName(String);
 
 impl TryFrom<String> for NewPersonName {
   type Error = &'static str;
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
-    if value.len() > 100 {
+    if value.len() <= 100 {
       Ok(NewPersonName(value))
     } else {
       Err("name is greater than permitted")
@@ -41,14 +52,14 @@ impl TryFrom<String> for NewPersonName {
   }
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Serialize, Debug, Deserialize, Clone)]
 pub struct NewPersonNick(String);
 
 impl TryFrom<String> for NewPersonNick {
   type Error = &'static str;
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
-    if value.len() > 32 {
+    if value.len() <= 32 {
       Ok(NewPersonNick(value))
     } else {
       Err("nick is greater than permitted")
@@ -56,14 +67,14 @@ impl TryFrom<String> for NewPersonNick {
   }
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Serialize, Debug, Deserialize, Clone)]
 pub struct Tech(String);
 
 impl TryFrom<String> for Tech {
   type Error = &'static str;
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
-    if value.len() > 32 {
+    if value.len() <= 32 {
       Ok(Tech(value))
     } else {
       Err("tech is greater than permitted")
@@ -77,87 +88,82 @@ impl From<Tech> for String {
   }
 }
 
-#[derive(Deserialize, Clone)]
-pub struct NewPerson {
-  #[serde(rename = "nome")]
-  pub name: NewPersonName,
-  #[serde(rename = "apelido")]
-  pub nick: NewPersonNick,
-  #[serde(rename = "nascimento", with = "date_format")]
-  pub birth_date: Date,
-  pub stack: Option<Vec<Tech>>,
+#[derive(Deserialize)]
+struct PersonSearchQuery {
+  #[serde(rename = "t")]
+  query: String,
 }
 
-type AppState = Arc<RwLock<HashMap<Uuid, Person>>>;
+type AppState = Arc<PostgresRepository>;
 
 #[tokio::main]
 async fn main() {
 
-  let mut people: HashMap<Uuid, Person> = HashMap::new();
+  let port = env::var("PORT")
+    .ok()
+    .and_then(|port| port.parse::<u16>().ok())
+    .unwrap_or(9999);
 
-  let person = Person {
-    id: Uuid::now_v7(),
-    name: "Yury Cavalcante".to_string(),
-    nick: "cavalcanteyury".to_string(),
-    birth_date: date!(1997 - 01 - 20),
-    stack: Some(vec!["Ruby".to_string(), "Rust".to_string()]),
-  };
+  let database_url = env::var("DATABASE_URL")
+    .unwrap_or(String::from("postgres://rinha:rinha@localhost:5432/rinha"));
 
-  println!("{:?}", &person.id);
+  let postgres_repo = persistence::PostgresRepository::connect(database_url).await;
 
-  people.insert(person.id, person);
-
-  let app_state: AppState = Arc::new(RwLock::new(people));
+  let app_state: AppState = Arc::new(postgres_repo);
 
   // build our application with a single route
   let app = Router::new()
-      .route("/pessoas", get(search_people))
-      .route("/pessoas/:id", get(find_person))
-      .route("/pessoas", post(create_person))
-      .route("/contagem-pessoas", get(count_people))
-      .with_state(app_state);
+    .route("/pessoas", get(search_people))
+    .route("/pessoas/:id", get(find_person))
+    .route("/pessoas", post(create_person))
+    .route("/contagem-pessoas", get(count_people))
+    .with_state(app_state);
 
 
   // run it with hyper on localhost:3000
-  axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-      .serve(app.into_make_service())
-      .await
-      .unwrap();
+  axum::Server::bind(&SocketAddr::from(([0, 0, 0, 0], port)))
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
 
-async fn search_people() -> impl IntoResponse {
-  (StatusCode::NOT_FOUND, "Busca de Pessoas")
+async fn search_people(
+  State(people): State<AppState>,
+  Query(PersonSearchQuery { query }): Query<PersonSearchQuery>,
+) -> impl IntoResponse {
+  match people.search_people(query).await {
+    Ok(people) => Ok(Json(people.clone())),
+    Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+  }
 }
 
 async fn find_person(
   State(people): State<AppState>,
   Path(person_id): Path<Uuid>,
 ) -> impl IntoResponse {
-  match people.read().await.get(&person_id) {
-    Some(person) => Ok(Json(person.clone())),
-    None => Err(StatusCode::NOT_FOUND),
-  }
+  match people.find_person(person_id).await {
+    Ok(Some(person)) => Ok(Json(person)),
+    Ok(None) => Err(StatusCode::NOT_FOUND),
+    Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+}
 }
 
 async fn create_person(
   State(people): State<AppState>,
   Json(new_person): Json<NewPerson>
 ) -> impl IntoResponse {
-  let id = Uuid::now_v7();
-
-  let new_person: Person = Person {
-    id,
-    name: new_person.name.0,
-    nick: new_person.nick.0,
-    birth_date: new_person.birth_date,
-    stack: new_person.stack.map(|stack: Vec<Tech>| stack.into_iter().map(String::from).collect())
-  };
-
-  people.write().await.insert(new_person.id, new_person.clone());
-
-  (StatusCode::OK, Json(new_person))
+  match people.create_person(new_person).await {
+    Ok(person) => Ok((StatusCode::CREATED, Json(person))),
+    Err(sqlx::Error::Database(err)) if err.is_unique_violation() => {
+        Err(StatusCode::UNPROCESSABLE_ENTITY)
+    }
+    Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+  }
 }
 
-async fn count_people() -> impl IntoResponse {
-  (StatusCode::NOT_FOUND, "Contagem de pessoas")
+async fn count_people(State(people): State<AppState>) -> impl IntoResponse {
+  match people.count_people().await {
+    Ok(count) => Ok(Json(count)),
+    Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+  }
 }
